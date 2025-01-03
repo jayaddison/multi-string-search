jayaddison/multi-string-search

commit 249fc659271bebfa1642201e463c742fc54ee511

Fixup: prefix matching from window start

src/multi_string_search/__init__.py
@@ -159,11 +159,13 @@ class FactorOracle:
                 continue
             finally:
                 assert advance >= 0
-                document = document[advance or 1:]  # advance past failed char
+                document = document[advance:]  # advance past failed char
             if state in terminals:
                 found |= {term for term in self.terms if document.startswith(term)}
                 if len(found) == len(self.terms):
                     return True
+            if advance == 0:
+                document = document[1:]
         return False
 
 

tests/fixtures.py
@@ -2,6 +2,7 @@ complete_subset_queries = (
   ["sample paragraph", "confirm the behaviour", "text search"],
   ["paragraph of text", "Set-Backwards-Oracle-Matching", "multi-pattern"],
   ["text"],
+  ["This is"],
 )
 
 overlapping_set_queries = (

commit 3b89bdc4671b5be6a71a340365ae6ab6737131e5

Fixup: prevent infinite loop on initial-char-match-failure case
Fixes bug introduced by commit 249fc659271bebfa1642201e463c742fc54ee511.

src/multi_string_search/__init__.py
@@ -156,7 +156,7 @@ class FactorOracle:
                         break
                     advance -= 1
             except KeyError:
-                continue
+                pass
             finally:
                 assert advance >= 0
                 document = document[advance:]  # advance past failed char